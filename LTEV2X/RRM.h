b6dd77012ba30775ef345dbefc4e27d59fe75e98
LiuYe <liuyehcf@163.com>
2016-11-15 13:29:06 +0800

    version_2016_11_15(加点注释)

--- LTEV2X/RRM.h
+++ LTEV2X/RRM.h
@@ -5,8 +5,60 @@
 //<RRM>: Radio Resource Management
 
 class RRM_Basic {
+	/*------------------域------------------*/
 public:
+	/*
+	* 当前的TTI时刻,指向系统的该参数
+	*/
+	int& m_TTI;
+
+	/*
+	* 系统配置参数,指向系统的该参数
+	*/
+	SystemConfig& m_Config;
+
+	/*
+	* RSU容器,指向系统的该参数
+	*/
+	RSU* m_RSUAry;
+
+	/*
+	* VeUE容器,指向系统的该参数
+	*/
+	VeUE* m_VeUEAry;
+
+	/*
+	* 事件容器,指向系统的该参数
+	*/
+	std::vector<Event>& m_EventVec;
+
+	/*
+	* 事件触发链表,指向系统的该参数
+	*/
+	std::vector<std::list<int>>& m_EventTTIList;
+
+	/*
+	* 吞吐率，外层下标为TTI，内层下标为RSUId,指向系统的该参数
+	*/
+	std::vector<std::vector<int>>& m_TTIRSUThroughput;
+
+													  
+	/*
+	* 其他模块调用时间记录
+	*/
+	long double m_GTTTimeConsume = 0;
+	long double m_WTTimeConsume = 0;
+public:
+	/*
+	* 默认构造函数定义为删除
+	*/
 	RRM_Basic() = delete;
+
+	/*
+	* 构造函数
+	* 该构造函数定义了该模块的视图
+	* 所有指针成员拷贝系统类中的对应成员指针，共享同一实体
+	*/
 	RRM_Basic(int &t_TTI, SystemConfig& t_Config, RSU* t_RSUAry, VeUE* t_VeUEAry, std::vector<Event>& t_EventVec, std::vector<std::list<int>>& t_EventTTIList, std::vector<std::vector<int>>& t_TTIRSUThroughput) :
 		m_TTI(t_TTI),
 		m_Config(t_Config),
@@ -16,23 +68,21 @@ public:
 		m_EventTTIList(t_EventTTIList),
 		m_TTIRSUThroughput(t_TTIRSUThroughput) {}
 
-	//数据成员
-	int& m_TTI;//当前的TTI时刻
-	SystemConfig& m_Config;//系统参数配置
-	RSU* m_RSUAry;//RSU容器
-	VeUE* m_VeUEAry;//VeUE容器
-	std::vector<Event>& m_EventVec;//事件容器
-	std::vector<std::list<int>>& m_EventTTIList;//事件触发链表，m_EventList[i]代表第i个TTI的事件表
-	std::vector<std::vector<int>>& m_TTIRSUThroughput;//吞吐率，外层下标为TTI，内层下标为RSUId
 
-	//第三方模块时间统计
-	long double m_GTTTimeConsume = 0;
-	long double m_WTTimeConsume = 0;
+	/*
+	* 初始化RSU VeUE内该单元的内部类
+	*/
+	virtual void initialize() = 0;
 
-	//接口
-	virtual void initialize() = 0;//初始化RSU VeUE内该单元的内部类
-	virtual void cleanWhenLocationUpdate() = 0;//当发生位置更新时，清除缓存的调度相关信息
-	virtual void schedule() = 0;//流程总控函数
+	/*
+	* 当发生位置更新时，清除缓存的调度相关信息
+	*/
+	virtual void cleanWhenLocationUpdate() = 0;
+
+	/*
+	* 流程总控函数
+	*/
+	virtual void schedule() = 0;
 };
 
 
