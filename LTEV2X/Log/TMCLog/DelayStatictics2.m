1b2b8022942dea7ed4741f0ce3918ac35db328e0
liuyehcf-Win <liuyehcf@163.com>
2017-10-22 12:12:03 +0800

    打包给吴禹博

--- LTEV2X/Log/TMCLog/DelayStatictics2.m
+++ LTEV2X/Log/TMCLog/DelayStatictics2.m
@@ -180,5 +180,5 @@ plot(centerPackageLossDistance,numPackageLossDistanceRate,'bo-','LineWidth',2);
 title('PDR','LineWidth',2);
 xlabel('Distance(m)','LineWidth',2);
 ylabel('Drop Rate','LineWidth',2);
-%axis([0 max(PackageLossDistance) 0 1]);
+axis([0 max(PackageLossDistance) 0 1]);
 grid on;
